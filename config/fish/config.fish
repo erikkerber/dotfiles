8ebbc8ef35dd9f5bd330623215c83152462666a6
Erik Kerber <ekerber@slack-corp.com>
2021-06-10 09:43:38 -0500

    Remove unneeded paths

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,23 +4,18 @@
 # Path to dotfiles repo
 set -x DOTFILES (dirname (readlink "$HOME/.config"))
 
-# Target stuff
-set -x DRONE_SERVER drone6.target.com
-
 #set -gx PATH \$PATH ~/
 set -gx PATH $HOME/.bin $PATH
 # abrew
 set -gx PATH /opt/homebrew/bin $PATH
-# ibrew
-set -gx PATH $HOME/usr/local/sbin $PATH
 # go
 set -gx PATH $HOME/go/bin $PATH
 # slack
 set -gx PATH $HOME/dev/slack/slack-objc/bin $PATH
 # rust
 set -Ua fish_user_paths $HOME/.cargo/bin
-# FZF
 
+# FZF
 set -gx FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*" --ansi'
 set -gx FZF_DEFAULT_COMMAND 'fd --type file --follow --hidden --exclude .git --color=always'
 set -Ux FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
@@ -42,5 +37,3 @@ end
 
 # Special one-off for scmpuff
 source $DOTFILES/**/fish/conf.d/scmpuff.fish
-
-set -g fish_user_paths "/opt/homebrew/opt/node@14/bin" $fish_user_paths
