ce0faff3e16313ce8011ee32dd7c1b230c006e59
Erik Kerber <ekerber@slack-corp.com>
2022-03-29 09:34:09 -0500

    No delay in dock autohide

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -35,7 +35,7 @@ defaults write com.apple.dt.Xcode ApplePersistenceIgnoreState -bool YES
 # Autohide quickly
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock autohide-delay -float 0.0
-defaults write com.apple.dock autohide-time-modifier -float 0.2
+defaults write com.apple.dock autohide-time-modifier -float 0.0
 
 defaults write com.apple.dock autohide-time-modifier -float 0.2
 
