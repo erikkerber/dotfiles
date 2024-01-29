87148759551f1b005e7bc012106cc38b50c27b68
Erik Kerber <ekerber@slack-corp.com>
2024-01-28 21:53:58 -0600

    Convert to Fish abbreviations

--- dot_config/private_fish/aliases.fish
+++ dot_config/private_fish/aliases.fish
@@ -1,8 +1,8 @@
 # Enable aliases to be sudo'ed
-alias sudo="sudo "
+abbr sudo "sudo "
 
 # Fish
-alias reload='source ~/.config/fish/config.fish'
+abbr reload 'source ~/.config/fish/config.fish'
 
 # Navigation
 function ..    ; cd .. ; end
@@ -10,143 +10,124 @@ function ...   ; cd ../.. ; end
 function ....  ; cd ../../.. ; end
 function ..... ; cd ../../../.. ; end
 
-function g        ; git $argv ; end
-function grep     ; command grep --color=auto $argv ; end
+function grep     ; command grep --color auto $argv ; end
 
-alias ls="exa"
+if type -q exa
+  abbr ls "exa"
+end
 
 # Git
-alias g="git"
-alias gti="git"
-alias push="git push"
-alias gcm="g co main"
-alias gcl="g co -"
-alias grbm="g rebase main"
-alias gap='g add .;g amendne; g punch'
-alias ga='g headpr' # git-pile helper
-alias ghs='g headpr --squash' # git-pile helper
-alias ggone="g branch -v | grep '\[gone\]' | awk '{ print \$1 }'"
-alias glo="g lo"
+if type -q git
+  abbr g 'git'
+  abbr gti 'git'
+  abbr push 'git push'
+  abbr gcm 'g co main'
+  abbr gcl 'g co -'
+  abbr grbm 'g rebase main'
+  abbr gap 'g add .;g amendne; g punch'
+  abbr ga 'g headpr' # git-pile helper
+  abbr ghs 'g headpr --squash' # git-pile helper
+  abbr ggone 'g branch -v | grep '\[gone\]' | awk '{ print \$1 }''
+  abbr glo 'g lo'
+  abbr gd 'git diff'
+  abbr gdc 'git diff --cached -w'
+  abbr gds 'git diff --staged -w'
+  abbr gsh 'git show'
+  abbr gl 'git l'
+end
 
 # Bazel
-alias b="bazel"
-alias br="bazel run"
-#alias bb="bazel build"
-alias bt="bazel test"
+if type -q bazel
+  abbr b 'bazel'
+  abbr br 'bazel run'
+  abbr bt 'bazel test'
+  abbr bb 'bazel build'
+  abbr bi 'bazel info'
+  abbr bbl 'bazel info bazel-bin'
+end
 
 # Vim
-alias v="nvim"
-alias vv="nvim ."
-
-alias chmox='chmod +x'
+if type -q nvim
+  abbr v 'nvim'
+  abbr vv 'nvim .'
+end
 
 # Brew
-alias cask='brew cask'
-alias brewu='brew update && brew upgrade && brew cleanup && brew doctor'
-alias ibrew='arch -x86_64 /usr/local/bin/brew'
-# Homebrew upgrade/date is confusing
-alias brew_update="brew -v update; brew upgrade --force-bottle --cleanup; brew cleanup; brew cask cleanup; brew prune; brew doctor"
-
-# *nix
-alias hosts='sudo $EDITOR /etc/hosts'
-alias fs="stat -f \"%z bytes\""
-alias tryst='cd (mktemp -d)'
-
-# Rosetta
-alias i="arch -x86_64"
+if type -q brew
+  abbr cask 'brew install --cask'
+  abbr brewu 'brew update && brew upgrade && brew cleanup && brew doctor'
+  abbr ibrew 'arch -x86_64 /usr/local/bin/brew'
+  # Homebrew upgrade/date is confusing
+  abbr brew_update 'brew -v update; brew upgrade --force-bottle --cleanup; brew cleanup; brew cask cleanup; brew prune; brew doctor'
+end
 
 # Networking. IP address, dig, DNS
-alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
-alias dig="dig +nocmd any +multiline +noall +answer"
+if type -q dig
+  abbr ip "dig +short myip.opendns.com @resolver1.opendns.com"
+  abbr dig "dig +nocmd any +multiline +noall +answer"
+end
 
-alias diskspace_report="df -P -kHl"
-alias free_diskspace_report="diskspace_report"
-
-# macOS
-
-# TODO / (ultralist)
-alias td='ultralist'
-alias tdc='ultralist complete'
-alias tda='ultralist add'
-alias tdd='ultralist list group:contexts'
-alias tddn='tdd context:Now'
-alias tdn='ultralist list group:contexts --notes'
-alias tdl='tdd'
-
-# Thermal throttling monitor
-alias thermlog="pmset -g thermlog"
+# Better formatted diskspace
+if type -q df
+  abbr diskspace "df -P -kHl"
+end
 
 # Archives
-alias mktar="tar -pvczf"
-alias untar="tar -zxvf"
+if type -q tar
+  abbr mktar 'tar -pvczf'
+  abbr untar 'tar -zxvf'
+end
 
-# Remove all items from the dock
-alias cleardock="defaults write com.apple.dock persistent-apps -array \"\" && killall Dock"
-
-# Git
-alias gd='git diff'
-alias gdc='git diff --cached -w'
-alias gds='git diff --staged -w'
-alias gsh='git show'
-alias gl='git l'
-
-# Quick Edits
-alias fe='v ~/.config/fish/config.fish'
-alias ae='v ~/.config/fish/aliases.fish'
-alias ve='v ~/.vimrc'
-alias ge='v ~/.gitconfig'
-alias be='v ~/.Brewfile'
-alias bce='v ~/.Brewfile.mac'
-alias bre='v ~/.bazelrc'
-alias pe='v ~/.vim/plugs.vim'
-alias te='v ~/.tmux.conf'
-alias ace='v ~/.config/alacritty/alacritty.yml'
+# Config quick Edits
+if type -q chezmoi
+  abbr fe 'chezmoi edit ~/.config/fish/config.fish'
+  abbr ae 'chezmoi edit ~/.config/fish/aliases.fish'
+  abbr ve 'chezmoi edit ~/.vimrc'
+  abbr ge 'chezmoi edit ~/.gitconfig'
+  abbr be 'v ~/.local/share/chezmoi/Brewfile'
+  abbr bce 'v ~/.local/share/chezmoi/Brewfile.mac'
+  abbr bre 'chezmoi ~/.bazelrc'
+  abbr pe 'chezmoi edit ~/.vim/plugs.vim'
+  abbr te 'chezmoi edit ~/.tmux.conf'
+  abbr ace 'chezmoi edit ~/.config/alacritty/alacritty.yml'
+end
 
 # Xcode
-alias xc-cleansims='xcrun simctl delete unavailable'
-alias xc-resignxcode='sudo codesign -f -s XcodeSigner' # Pass Xcode path
-alias xc-provfiles='cd ~/Library/MobileDevice/Provisioning\ Profiles'
-alias xc-dd='cd ~/Library/Developer/Xcode/DerivedData'
-alias xc-rmdd='rm -fr ~/Library/Developer/Xcode/DerivedData'
-alias xc-buildnum='xcodebuild -version | tail -1 | cut -d " " -f3'
+if type -q xcrun
+  abbr xc-cleansims 'xcrun simctl delete unavailable'
+  abbr xc-resignxcode 'sudo codesign -f -s XcodeSigner' # Pass Xcode path
+  abbr xc-provfiles 'cd ~/Library/MobileDevice/Provisioning\ Profiles'
+  abbr xc-dd 'cd ~/Library/Developer/Xcode/DerivedData'
+  abbr xc-rmdd 'rm -fr ~/Library/Developer/Xcode/DerivedData'
+  abbr xc-buildnum 'xcodebuild -version | tail -1 | cut -d " " -f3'
+  # iOS Simulator
+  abbr xc-deeplink 'xcrun simctl openurl booted'
+  abbr xc-devices 'xcrun simctl list devices'
+  abbr xc-killsim 'killall "com.apple.CoreSimulator.CoreSimulatorService" && killall "Simulator"'
+end
 
 # Static Analysis
-alias methoddump='otool -ov'
-
-# iOS Simulator
-alias deeplink='xcrun simctl openurl booted'
-
-# Bazel
-alias bazelbinloc='bazel info bazel-bin'
-alias bi='bazel info'
+if type -q xcrun
+  abbr otool-methods 'otool -ov'
+end
 
 # Docker
-alias dockerkillall='docker kill (docker ps -q)'
-alias dockerdac='docker rm (docker ps -a -q)'
-alias dockerdai='docker rmi (docker images -q)'
+if type -q docker
+  abbr dockerkillall 'docker kill (docker ps -q)'
+  abbr dockerdac 'docker rm (docker ps -a -q)'
+  abbr dockerdai 'docker rmi (docker images -q)'
+end
 
-# AG
-alias ag='ag --follow --hidden --ignore .git --ignore derivedData --ignore Carthage'
+# ag
+if type -q ag
+  abbr brew 'ag --follow --hidden --ignore .git'
+end
 
 # JSON Pretty Print
-alias jpp='pbpaste | json_pp | bat'
-
-# Simctl
-alias xcdevices='xcrun simctl list devices'
-alias killsim='killall "com.apple.CoreSimulator.CoreSimulatorService" && killall "Simulator"'
+if type -q json_pp
+  abbr jpp 'pbpaste | json_pp | bat'
+end
 
 # macOS
-alias makespacer='defaults write com.apple.dock persistent-apps -array-add \'{"tile-type"="spacer-tile";}\'; killall Dock'
-alias daemonblame='launchctl list | grep -v com.apple'
-alias kextblame='kextstat -l | grep -v com.apple'
+abbr daemonblame 'launchctl list | grep -v com.apple'
 
-# Secrets
-
-## Fish-friendly signin to 1Password. Sets OP_SESSION_my env var
-alias ops='set -x OP_SESSION_my (op signin|sed -n \'s/.*OP_SESSION_my=//p\'|tr -d \'"\')'
-
-## After login above, source my secrets file
-alias opsource='op get document 7dgga4l2zndutiafauklhwfweq | source'
-
-# Reminders
-alias rs='reminders add Slack'
