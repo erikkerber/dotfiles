9c3d8810180fac0847df9d3f6751787cc0746b8e
Erik Kerber <ekerber@slack-corp.com>
2025-04-27 21:28:03 -0500

    Use nvim api for keymaps

--- dot_config/nvim/lua/setup.lua
+++ dot_config/nvim/lua/setup.lua
@@ -1,44 +1,48 @@
 -- Map the leader key to <Space>
-vim.api.nvim_set_keymap("n", "<Space>", "", {})
+vim.keymap.set("n", "<Space>", "", {})
 vim.g.mapleader = " "
 
+
+local keyset = vim.keymap.set
+keyset("n", "<leader>sr", ":%s/<<C-r><C-w>>//g<Left><Left>")
+
 -- Switch to the last file
-vim.api.nvim_set_keymap('n', '<leader><leader>', '<C-^>', {noremap = true})
+vim.keymap.set('n', '<leader><leader>', '<C-^>', {noremap = true})
 
 -- Clear highlight after a search
-vim.api.nvim_set_keymap('n', '<leader>c', ':let @/=""<CR>', {silent = true, noremap = true})
+vim.keymap.set('n', '<leader>c', ':let @/=""<CR>', {silent = true, noremap = true})
 
 -- Save and Quit cheat codes
-vim.api.nvim_set_keymap('n', 'Q', ':quit<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', 'W', ':write<CR>', {noremap = true})
+vim.keymap.set('n', 'Q', ':quit<CR>', {noremap = true})
+vim.keymap.set('n', 'W', ':write<CR>', {noremap = true})
 
 -- Delete without yanking
-vim.api.nvim_set_keymap('n', '<leader>d', '"_d', {noremap = true})
-vim.api.nvim_set_keymap('v', '<leader>d', '"_d', {noremap = true})
+vim.keymap.set('n', '<leader>d', '"_d', {noremap = true})
+vim.keymap.set('v', '<leader>d', '"_d', {noremap = true})
 
 -- Replace currently selected text with default register without yanking it
-vim.api.nvim_set_keymap('v', '<leader>p', '"_dP', {noremap = true})
+vim.keymap.set('v', '<leader>p', '"_dP', {noremap = true})
 
 -- Jump to definition
-vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', {silent = true})
+vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', {silent = true})
 -- Open git commits with custom command ':Commits'
-vim.api.nvim_set_keymap('n', '<Leader>g', ':Commits<CR>', {silent = true})
+vim.keymap.set('n', '<Leader>g', ':Commits<CR>', {silent = true})
 -- Jump to declaration
-vim.api.nvim_set_keymap('n', '<leader>c', '<cmd>lua vim.lsp.buf.declaration()<CR>', {silent = true})
+vim.keymap.set('n', '<leader>c', '<cmd>lua vim.lsp.buf.declaration()<CR>', {silent = true})
 -- Find implementations
-vim.api.nvim_set_keymap('n', '<leader>i', '<cmd>lua vim.lsp.buf.implementation()<CR>', {silent = true})
+vim.keymap.set('n', '<leader>i', '<cmd>lua vim.lsp.buf.implementation()<CR>', {silent = true})
 -- Find references
-vim.api.nvim_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', {silent = true})
+vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', {silent = true})
 -- Set location list with diagnostics
-vim.api.nvim_set_keymap('n', '<leader>l', '<cmd>lua vim.lsp.diagnostic.setloclist()<CR>', {silent = true})
+vim.keymap.set('n', '<leader>l', '<cmd>lua vim.lsp.diagnostic.setloclist()<CR>', {silent = true})
 -- Invoke code actions
-vim.api.nvim_set_keymap('n', '<leader>a', '<cmd>lua vim.lsp.buf.code_action()<CR>', {silent = true})
+vim.keymap.set('n', '<leader>a', '<cmd>lua vim.lsp.buf.code_action()<CR>', {silent = true})
 -- Open diagnostic float
-vim.api.nvim_set_keymap('n', '<Leader>d', '<cmd>lua vim.diagnostic.open_float()<CR>', {silent = true})
+vim.keymap.set('n', '<Leader>d', '<cmd>lua vim.diagnostic.open_float()<CR>', {silent = true})
 -- Formatting
-vim.api.nvim_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', {silent = true})
+vim.keymap.set('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', {silent = true})
 -- Show hover
-vim.api.nvim_set_keymap('n', '<leader>h', '<cmd>lua vim.lsp.buf.hover()<CR>', {silent = true})
+vim.keymap.set('n', '<leader>h', '<cmd>lua vim.lsp.buf.hover()<CR>', {silent = true})
 -- Rename symbol
-vim.api.nvim_set_keymap('n', '<leader>r', '<cmd>lua vim.lsp.buf.rename()<CR>', {silent = true})
+vim.keymap.set('n', '<leader>r', '<cmd>lua vim.lsp.buf.rename()<CR>', {silent = true})
 
