c4ff906d2b1e9201098c886e3ba47ccabd300a1d
Erik Kerber <ekerber@slack-corp.com>
2026-02-17 16:48:26 -0600

    Bun

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -10,6 +10,7 @@ set -x ANDROID_HOME ~/Library/Android/sdk
 fish_add_path $ANDROID_HOME/platform-tools 
 fish_add_path $ANDROID_HOME/emulator 
 fish_add_path $ANDROID_HOME/cmdline-tools/latest/bin
+fish_add_path ~/.bun/bin
 
 # .config setting (for several apps including lazygit)
 set -q XDG_CONFIG_HOME; or set -g XDG_CONFIG_HOME "$HOME/.config"
