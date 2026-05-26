c232a7c4fd569c3748acdd7dbc20c72153aedebd
Erik Kerber <erik.kerber@reddit.com>
2026-05-26 09:09:44 -0500

    Fix keybindings

--- dot_config/private_fish/functions/fish_user_key_bindings.fish
+++ dot_config/private_fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,12 @@
 function fish_user_key_bindings
-#  fish_vi_key_bindings
-  bind -M visual \t accept-autosuggestion
-#  bind -M insert \t accept-autosuggestion
+    fish_vi_key_bindings
+    functions -q fzf_configure_bindings; and fzf_configure_bindings \
+        --directory=ctrl-t \
+        --git_log=ctrl-o \
+        --git_status=ctrl-g \
+        --history=ctrl-r \
+        --processes=ctrl-p \
+        --variables=ctrl-v
+    bind -M visual \t accept-autosuggestion
+    #  bind -M insert \t accept-autosuggestion
 end
